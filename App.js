6f19baef467a87aca7ae5509e7567aa455d162f7
Freddie Swift <freddie9117@live.co.uk>
2020-03-06 15:37:21 +0000

    Added /chits end point
    Chits are now displayed on the flat list, cleaned up layout aswell

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 class chittr extends Component {
     constructor(props) {
         super(props);
 		this.state = {
-		  names: [
-			 {'name': 'Ben', 'id': 1},
-			 {'name': 'Susan', 'id': 2},
-			 {'name': 'Robert', 'id': 3},
-			 {'name': 'Mary', 'id': 4},
-			 {'name': 'Daniel', 'id': 5},
-			 {'name': 'Laura', 'id': 6},
-			 {'name': 'John', 'id': 7},
-			 {'name': 'Debra', 'id': 8},
-			 {'name': 'Aron', 'id': 9},
-			 {'name': 'Ann', 'id': 10},
-			 {'name': 'Steve', 'id': 11},
-			 {'name': 'Olivia', 'id': 12}
-		  ]
+		  isLoading: true,
+		  chitsData: []
 	   }
     }
     render() {
+		if(this.state.isLoading){
+			return(
+				<View>
+					<ActivityIndicator/>
+				</View>
+			)
+		}
         return (
             <View style={styles.container}>
                 <View style={styles.headerBar}>
@@ -30,12 +25,19 @@ class chittr extends Component {
                 </View>
 				<View style = {styles.chittList}>
 					<FlatList
-						data={this.state.names}
+						data={this.state.chitsData}
 						renderItem={({item}) => 
 							<View style={styles.chitt}>
-								<Text style = {styles.chittText}>{item.name}</Text>
+								<View style>
+									<Text style = {styles.userInfoText}>{item.user.given_name + " " + item.user.family_name}</Text>
+								</View>
+								<View>
+									<Text style = {styles.chittText}>{item.chit_content}</Text>
+								</View>
+								
 							</View>
 						}
+						//keyExtractor={({id}, index) => id}
 						keyExtractor={item=>item.id}
 					/>
 				</View>
@@ -45,29 +47,59 @@ class chittr extends Component {
 
         );
     }
+	
+	getChits(){
+		return fetch('http://10.0.2.2:3333/api/v0.0.5/chits')
+			.then((response) => response.json())
+			.then((responseJson) => {
+				this.setState({
+					isLoading: false,
+					chitsData: responseJson,
+				});
+			})
+			.catch((error) => {console.log(error);});
+	}
+	
+	componentDidMount(){
+		this.getChits();
+	}
 }
 const styles = StyleSheet.create({
+	//holds the whole page
     container: {
 		flex: 1,
         flexDirection: 'column',
         alignItems: 'stretch'
     },
 	
+	//user info and time stamp at top of chit
+	/* chitUserInfoContainer: {
+		backgroundColor: 'palevioletred',
+	}, */
+	
+	//content of chit 
+	/* chitContentContainer: {
+		
+		
+	}, */
+	
+	//the whole chit
 	chitt: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		padding: 30,
-		margin: 2,
-		borderColor: 'black',
+		flexDirection: 'column',
+		alignItems: 'stretch',
+		margin: 5,
+		padding: 5,
+		borderColor: 'grey',
 		borderWidth: 1,
 		backgroundColor: 'white'
 	},
 	
+	// flatlist to hold chitts
 	chittList: {
 		flex: 8
 	},
 
+	// header at top of screen
     headerBar: {
 		flex: 1,
         flexDirection: 'row',
@@ -84,8 +116,15 @@ const styles = StyleSheet.create({
         
     },
 	
+	userInfoText: {
+		fontSize: 15,
+		paddingLeft: 5
+	},
+	
+	//chit content text
 	chittText: {
-		fontSize: 20
+		fontSize: 25,
+		paddingLeft: 5
 	},
 
     chittrHeaderText: {
